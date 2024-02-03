793db1bdb214d2790a3b3d502802eee2c377d657
moremaryna <mary.seleznova@gmail.com>
2024-02-03 21:30:42 +0000

    updeated requirements

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development servers
     app.run()
+    
+port_nr = int(os.environ.get("PORT", 5001))
+app.run(port=port_nr, host='0.0.0.0')

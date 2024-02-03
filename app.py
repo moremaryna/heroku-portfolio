eee1acc7920621034d3d363e244cd32a69363a2c
moremaryna <mary.seleznova@gmail.com>
2024-02-03 21:37:41 +0000

    updeated requirements

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,5 +12,5 @@ if __name__ == '__main__':
     # on the local development servers
     app.run()
     
-port_nr = int(os.environ.get("PORT", 5001))
-app.run(port=port_nr, host='0.0.0.0')
+port = int(os.environ.get("PORT", 5000))
+app.run(port=port, host='0.0.0.0')

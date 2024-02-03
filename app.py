c9c7e4d3492e948d82ee93c9f3dbd94ce305a97a
moremaryna <mary.seleznova@gmail.com>
2024-02-03 12:38:14 +0000

    updated app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def hello():
     return "Hello, it's me!"
 
 if __name__ == '__main__':
-    import os
-    #Bind to PORT if defined, otherwise default to 5000
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # run() method of Flask class runs the application
+    # on the local development servers
+    app.run()

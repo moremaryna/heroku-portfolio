07092ce0fdeeb9590ea97c089d1fb2ddf88910db
moremaryna <mary.seleznova@gmail.com>
2024-02-05 00:02:07 +0000

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, it's me!"
+    return "Hello, it's me again!"
 
 if __name__ == '__main__':
     # run() method of Flask class runs the application
